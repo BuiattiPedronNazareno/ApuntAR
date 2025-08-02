42b042433cb2999a1509a7c599e89b396374f7c8
BuiattiPedronNazareno <buiattipedronazareno@gmail.com>
2025-08-02 12:12:52 -0300

    definir estructura

---
+++ apuntarservidor/src/main/java/com/apuntar/servidor/ApuntarservidorApplication.java
@@ -0,0 +1,13 @@
+package com.apuntar.servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuntarservidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApuntarservidorApplication.class, args);
+	}
+
+}
---
+++ apuntarservidor/src/test/java/com/apuntar/servidor/ApuntarservidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apuntar.servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApuntarservidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
